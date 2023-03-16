6ed734b945167bd86b833c27d0df48cb040f4566
Gianna Mastro <giannamastro02@gmail.com>
2023-03-16 15:39:30 -0400

    index.html

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+const button = document.createElement('button');
+button.innerText = 'Click me!';
+button.id= 'surprise'
+button.addEventListener('click', () => {
+    alert('Welcome! Feel free to take a look around!')
+});
+document.body.appendChild(button);
+
+//savory.addEventListener('mouseOver')
+//Create a mouseover event listener to change the appearance of the buttons when you hover over them instead of using the CSS 'hover' function
+//Create a function to say 'hello ${name} to page visitors when prompted to enter their name
