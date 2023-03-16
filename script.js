45c04e2d2057ab24ea15503417024fb321007593
Gianna Mastro <giannamastro02@gmail.com>
2023-03-16 17:42:42 -0400

    index.html

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-const button = document.createElement('button');
-button.innerText = 'Click me!';
-button.id= 'surprise'
-button.addEventListener('click', () => {
-    alert('Welcome! Feel free to take a look around!')
-});
-document.body.appendChild(button);
+function recipe() {
+document.getElementsByClassName("recipe").style.backgroundColor = "#abd7eb";
+}
+recipe();
 
 //savory.addEventListener('mouseOver')
 //Create a mouseover event listener to change the appearance of the buttons when you hover over them instead of using the CSS 'hover' function
